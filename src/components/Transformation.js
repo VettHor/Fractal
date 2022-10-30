f0b847c8970bc4583bbb553d315fd3aeff346128
VettHor <horbovyivitalyi@gmail.com>
2022-10-30 14:37:20 +0200

    Added function that draws figure on canvas

--- src/components/Transformation.js
+++ src/components/Transformation.js
@@ -9,7 +9,10 @@ export const Transformation = () => {
     const canvas = useRef(null);
     const [isActivePopout, setActivePopout] = useState(false);
     const [openPopout, setOpenPopout] = useState(false);
-    const [point1X, SetPoint1X] = useState(0);
+    const [point1, SetPoint1] = useState({x : '', y : ''});
+    const [point2, SetPoint2] = useState({x : '', y : ''});
+    const [point3, SetPoint3] = useState({x : '', y : ''});
+    const [point4, SetPoint4] = useState({x : '', y : ''});
 
     useEffect(() => {
         let context = canvas.current.getContext("2d");
@@ -37,7 +40,7 @@ export const Transformation = () => {
 
         context.moveTo(canvas.current.width / 2, 0);
         context.lineTo(canvas.current.width / 2, canvas.current.height);
-        context.strokeStyle = "#ff0000";
+        context.strokeStyle = "#CB2E81";
         context.stroke();
 
         context.beginPath();
@@ -48,12 +51,12 @@ export const Transformation = () => {
         context.moveTo(canvas.current.width - 25, canvas.current.height / 2 - 15);
         context.lineTo(canvas.current.width, canvas.current.height / 2);
         context.lineTo(canvas.current.width - 25, canvas.current.height / 2 + 15);
-        context.strokeStyle = "#ff0000";
+        context.strokeStyle = "#CB2E81";
         context.stroke();
 
         context.beginPath();
         context.font = "20px serif"
-        context.fillStyle = "#ff0000";
+        context.fillStyle = "black";
         for(var i = 5; i <= canvas.current.width; i += 35) {
             if(i < canvas.current.width - 35) {
                 context.moveTo(i, canvas.current.height / 2 - 3);
@@ -61,7 +64,7 @@ export const Transformation = () => {
                 if(i !== canvas.current.width / 2)
                     context.fillText((i - 5) / 35 - 11, i - 5, canvas.current.height / 2 + 24);
                 else 
-                context.fillText(0, i + 3, canvas.current.height / 2 + 24);
+                    context.fillText(0, i + 3, canvas.current.height / 2 + 24);
             }
 
             if(i !== 5) {
@@ -71,13 +74,46 @@ export const Transformation = () => {
                     context.fillText(-(i - 5) / 35 + 11, canvas.current.width / 2 + 8, i + 5);
             }
         }
-        context.strokeStyle = "#ff0000";
+        context.strokeStyle = "#CB2E81";
         context.stroke();
     }
 
     const setPopoutState = (state) => {
         setActivePopout(state)
         setOpenPopout(!state);
+    }
+
+    const validateInputs = () => {
+        let isBreak = false;
+        [point1, point2, point3, point4].map((point, i) => {
+            if(isBreak) return;
+            if(point.x === '') {
+                document.getElementById(`p${i + 1}X`).reportValidity();
+                isBreak = true;
+                return;
+            }
+            if(point.y === '') {
+                isBreak = true;
+                document.getElementById(`p${i + 1}Y`).reportValidity();
+                return;
+            }
+        })
+        drawParallelogram();
+    }
+
+    const drawParallelogram = () => {
+        const currZoom = 35;
+        let context = canvas.current.getContext("2d");
+        context.beginPath();
+        context.moveTo(Number(point1.x * currZoom + canvas.current.width / 2), Number(-point1.y * currZoom  + canvas.current.height / 2));
+        context.lineTo(Number(point2.x * currZoom + canvas.current.width / 2), Number(-point2.y * currZoom + canvas.current.height / 2));
+        context.lineTo(Number(point4.x * currZoom + canvas.current.width / 2), Number(-point4.y * currZoom + canvas.current.height / 2));
+        context.lineTo(Number(point3.x * currZoom + canvas.current.width / 2), Number(-point3.y * currZoom + canvas.current.height / 2));
+        context.lineTo(Number(point1.x * currZoom + canvas.current.width / 2), Number(-point1.y * currZoom  + canvas.current.height / 2));
+        context.strokeStyle = "#CB2E81";
+        context.fillStyle = "rgb(203, 46, 129, 0.2)";
+        context.stroke();
+        context.fill();
     }
 
     return(
@@ -106,19 +142,19 @@ export const Transformation = () => {
                                         />
                                     })}
                                     <Row className="justify-content-center">
-                                        <div className='bg-white border-custom-line w-50'>
+                                        <div className='bg-white border-custom-line transformation-position transformation-position-1'>
                                             <Form.Group>
                                                 <Row className="justify-content-center">
                                                     <div className='d-flex input-point parallelogram-input-point-1'>
-                                                        <h2 className='transformation-input-h2'>(</h2>
+                                                        <h2 className='transformation-input-h2 input-point-1-h2-margin'>(</h2>
                                                         <Form.Control
                                                             className="transformation-input fs-3 rounded-0 dotted-input-transformation text-center"
-                                                            placeholder='1'
+                                                            placeholder='0'
                                                             type="number"
-                                                            id='numerator'
-                                                            name="numerator"
+                                                            id='p1X'
+                                                            name="p1X"
                                                             pattern="[1-9][0-9]*"
-                                                            onChange={(event) => SetPoint1X(event.target.value)}
+                                                            onChange={(event) => SetPoint1({x : event.target.value, y : point1.y})}
                                                             onInvalid={e => e.target.setCustomValidity('Введіть чисельник дробу, що задає відношення поділу')}
                                                             onInput={e => e.target.setCustomValidity('')}
                                                             required
@@ -126,12 +162,123 @@ export const Transformation = () => {
                                                         <h2 className='transformation-input-h2'>;</h2>
                                                         <Form.Control
                                                             className="transformation-input fs-3 rounded-0 dotted-input-transformation text-center"
-                                                            placeholder='1'
+                                                            placeholder='0'
                                                             type="number"
-                                                            id='numerator'
-                                                            name="numerator"
+                                                            id='p1Y'
+                                                            name="p1Y"
                                                             pattern="[1-9][0-9]*"
-                                                            onChange={(event) => SetPoint1X(event.target.value)}
+                                                            onChange={(event) => SetPoint1({x : point1.x, y : event.target.value})}
+                                                            onInvalid={e => e.target.setCustomValidity('Введіть чисельник дробу, що задає відношення поділу')}
+                                                            onInput={e => e.target.setCustomValidity('')}
+                                                            required
+                                                        />
+                                                        <h2 className='transformation-input-h2'>)</h2>
+                                                    </div>
+                                                </Row>
+                                            </Form.Group>
+                                        </div>
+                                    </Row>
+                                    <Row className="justify-content-center">
+                                        <div className='bg-white border-custom-line transformation-position transformation-position-2'>
+                                            <Form.Group>
+                                                <Row className="justify-content-center">
+                                                    <div className='d-flex input-point parallelogram-input-point-1'>
+                                                        <h2 className='transformation-input-h2 input-point-1-h2-margin'>(</h2>
+                                                        <Form.Control
+                                                            className="transformation-input fs-3 rounded-0 dotted-input-transformation text-center"
+                                                            placeholder='0'
+                                                            type="number"
+                                                            id='p2X'
+                                                            name="p2X"
+                                                            pattern="[1-9][0-9]*"
+                                                            onChange={(event) => SetPoint2({x : event.target.value, y : point2.y})}
+                                                            onInvalid={e => e.target.setCustomValidity('Введіть чисельник дробу, що задає відношення поділу')}
+                                                            onInput={e => e.target.setCustomValidity('')}
+                                                            required
+                                                        />
+                                                        <h2 className='transformation-input-h2'>;</h2>
+                                                        <Form.Control
+                                                            className="transformation-input fs-3 rounded-0 dotted-input-transformation text-center"
+                                                            placeholder='0'
+                                                            type="number"
+                                                            id='p2Y'
+                                                            name="p2Y"
+                                                            pattern="[1-9][0-9]*"
+                                                            onChange={(event) => SetPoint2({x : point2.x, y : event.target.value})}
+                                                            onInvalid={e => e.target.setCustomValidity('Введіть чисельник дробу, що задає відношення поділу')}
+                                                            onInput={e => e.target.setCustomValidity('')}
+                                                            required
+                                                        />
+                                                        <h2 className='transformation-input-h2'>)</h2>
+                                                    </div>
+                                                </Row>
+                                            </Form.Group>
+                                        </div>
+                                    </Row>
+                                    <Row className="justify-content-center">
+                                        <div className='bg-white border-custom-line transformation-position transformation-position-3'>
+                                            <Form.Group>
+                                                <Row className="justify-content-center">
+                                                    <div className='d-flex input-point parallelogram-input-point-1'>
+                                                        <h2 className='transformation-input-h2 input-point-1-h2-margin'>(</h2>
+                                                        <Form.Control
+                                                            className="transformation-input fs-3 rounded-0 dotted-input-transformation text-center"
+                                                            placeholder='0'
+                                                            type="number"
+                                                            id='p3X'
+                                                            name="p3X"
+                                                            pattern="[1-9][0-9]*"
+                                                            onChange={(event) => SetPoint3({x : event.target.value, y : point3.y})}
+                                                            onInvalid={e => e.target.setCustomValidity('Введіть чисельник дробу, що задає відношення поділу')}
+                                                            onInput={e => e.target.setCustomValidity('')}
+                                                            required
+                                                        />
+                                                        <h2 className='transformation-input-h2'>;</h2>
+                                                        <Form.Control
+                                                            className="transformation-input fs-3 rounded-0 dotted-input-transformation text-center"
+                                                            placeholder='0'
+                                                            type="number"
+                                                            id='p3Y'
+                                                            name="p3Y"
+                                                            pattern="[1-9][0-9]*"
+                                                            onChange={(event) => SetPoint3({x : point3.x, y : event.target.value})}
+                                                            onInvalid={e => e.target.setCustomValidity('Введіть чисельник дробу, що задає відношення поділу')}
+                                                            onInput={e => e.target.setCustomValidity('')}
+                                                            required
+                                                        />
+                                                        <h2 className='transformation-input-h2'>)</h2>
+                                                    </div>
+                                                </Row>
+                                            </Form.Group>
+                                        </div>
+                                    </Row>
+                                    <Row className="justify-content-center">
+                                        <div className='bg-white border-custom-line transformation-position transformation-position-4'>
+                                            <Form.Group>
+                                                <Row className="justify-content-center">
+                                                    <div className='d-flex input-point parallelogram-input-point-1'>
+                                                        <h2 className='transformation-input-h2 input-point-1-h2-margin'>(</h2>
+                                                        <Form.Control
+                                                            className="transformation-input fs-3 rounded-0 dotted-input-transformation text-center"
+                                                            placeholder='0'
+                                                            type="number"
+                                                            id='p4X'
+                                                            name="p4X"
+                                                            pattern="[1-9][0-9]*"
+                                                            onChange={(event) => SetPoint4({x : event.target.value, y : point4.y})}
+                                                            onInvalid={e => e.target.setCustomValidity('Введіть чисельник дробу, що задає відношення поділу')}
+                                                            onInput={e => e.target.setCustomValidity('')}
+                                                            required
+                                                        />
+                                                        <h2 className='transformation-input-h2'>;</h2>
+                                                        <Form.Control
+                                                            className="transformation-input fs-3 rounded-0 dotted-input-transformation text-center"
+                                                            placeholder='0'
+                                                            type="number"
+                                                            id='p4Y'
+                                                            name="p4Y"
+                                                            pattern="[1-9][0-9]*"
+                                                            onChange={(event) => SetPoint4({x : point4.x, y : event.target.value})}
                                                             onInvalid={e => e.target.setCustomValidity('Введіть чисельник дробу, що задає відношення поділу')}
                                                             onInput={e => e.target.setCustomValidity('')}
                                                             required
@@ -145,7 +292,7 @@ export const Transformation = () => {
                                     <Form.Group className="mt-1">
                                         <Row className="justify-content-center">
                                             <span className='navbar-button navbar-button-slide home-button justify-content-center'>
-                                                <button className='calc-button' type="button" onClick={() => function() {}}>
+                                                <button className='calc-button' type="button" onClick={() => validateInputs()}>
                                                     <span>Save ratio</span>
                                                 </button>
                                             </span>
